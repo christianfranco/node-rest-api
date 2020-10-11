7d6f4ab8ecdd8b306898890657fc27c1c92656b7
Christian Franco <christian.franco@sap.com>
2020-10-11 18:21:55 +0100

    added validations

--- spec/service/stockLevelService.spec.js
+++ spec/service/stockLevelService.spec.js
@@ -12,13 +12,25 @@ describe('Test stockLevelService create: ', () => {
         await integrationTest.disconnectDB();
     });
 
-    it('create success', async () => {
+    it('success', async () => {
         let newStock = await stockLevelService.createStockLevel(buildStockLevel("Test", 30, Date.now()));
 
         expect(newStock.productName).toBe('Test');
     });
 
-    it('create error empty', () => {
-        expect(() => stockLevelService.createStockLevel('')).toThrow(new Error('stockLevelData cannot be empty.'));
+    it('error empty', () => {
+        expect(() => stockLevelService.createStockLevel(null)).toThrow(new Error('stockLevelData cannot be null.'));
+    });
+
+    it('error length', () => {
+        expect(() => stockLevelService.createStockLevel({productName:"ts"})).toThrow(new Error('ProductName length must be between 3 and 60.'));
+    });
+
+    it('error quantity negative', () => {
+        expect(() => stockLevelService.createStockLevel({productName:"test", quantity: -1})).toThrow(new Error('Quantity must be between 0 and 20_000_000.'));
+    });
+
+    it('error quantity overflow', () => {
+        expect(() => stockLevelService.createStockLevel({productName:"test", quantity: 20000001})).toThrow(new Error('Quantity must be between 0 and 20_000_000.'));
     });
 });
--- src/service/stockLevelDataValidator.js
+++ src/service/stockLevelDataValidator.js
@@ -1,11 +1,25 @@
-const CHAIN = [validateEmpty];
+const CHAIN = [validateEmpty, validateProductNameProperty, validateNameLength, validateQuantity];
 
 function validateStockLevelData(stockLevelData) {
     CHAIN.forEach((validate) => validate(stockLevelData));
 }
 
 function validateEmpty(stockLevelData) {
-    if (!stockLevelData || stockLevelData === '') throw new Error('stockLevelData cannot be empty.');
+    if (!stockLevelData) throw new Error('stockLevelData cannot be null.');
 }
 
-module.exports = {validateStockLevelData};
+function validateProductNameProperty(stockLevelData) {
+    if (!stockLevelData.productName) throw new Error('stockLevelData must have a productName.');
+}
+
+function validateNameLength(stockLevelData) {
+    var name = stockLevelData.productName;
+    if (!name || !(name.length >= 3 && name.length <= 60)) throw new Error('ProductName length must be between 3 and 60.');
+}
+
+function validateQuantity(stockLevelData) {
+    var quantity = stockLevelData.quantity;
+    if (!quantity || !(quantity >= 0 && quantity <= 20000000)) throw new Error('Quantity must be between 0 and 20_000_000.');
+}
+
+module.exports = { validateStockLevelData };
